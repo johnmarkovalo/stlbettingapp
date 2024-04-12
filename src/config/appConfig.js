3333a8dd534dc2e415076c10c6ae0ddd7139edaa
John Mark Ovalo <john.ovalo@vocphone.com>
2024-04-12 21:48:38 +0800

    refactor: change ip address

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -2,7 +2,8 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import DeviceInfo from 'react-native-device-info';
 
 export const appConfig = {
-  apiUrl: 'http://zian-api-v1.philippinestl.com/api/v2/',
+  // apiUrl: 'http://zian-api-v1.philippinestl.com/api/v2/', //Staging server
+  apiUrl: 'http://zian-api-v2.philippinestl.com/api/v2/', //Prod server
   environment: () => {
     if (__DEV__) return 'development';
 
