b94ee6ce0a6d79f44a74ec8c8d59d185d3e9d327
John Mark Ovalo <johnmarkovalo@gmail.com>
2026-01-09 00:07:30 +0800

    chore: increment version code to 19 and version name to 2.9 in build.gradle

--- src/helper/index.ts
+++ src/helper/index.ts
@@ -3,3 +3,5 @@ export * from './api';
 export * from './printer';
 export * from './localNotification';
 export * from './android-permissions';
+export * from './batchProcessor';
+export * from './apiQueue';
